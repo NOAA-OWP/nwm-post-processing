32d1f970f0d0cf49007b937cf9aa15e6bf32e450
christopher.tubbs <christopher.tubbs@noaa.gov>
2025-07-24 13:13:37 -0500

    Added a file that may be sourced to apply environment variable overrides

---
+++ environment.sh
@@ -0,0 +1,55 @@
+######################################################################################
+#
+# A store for optional environment variable configuration
+#
+#   Uncomment and configure variables prior to sourcing prior to calling post-process
+#
+######################################################################################
+
+# An override for Whether to run in DEBUG mode
+#export PP_debug="False"
+
+# An override for The format with which to print dates
+#export PP_date_format="%Y-%m-%d %H:%M:%S%z"
+
+# An override for The default format for log messages
+#export PP_log_format="[%(asctime)s] %(levelname)s %(name)s: %(message)s"
+
+# An override for The path to a file that details specific loggers to keep quiet and only log errors
+#export PP_loggers_to_quiet="resources/log_level_override"
+
+# An override for The path to a json log configuration
+#export PP_json_log_path=
+
+# An override for The path to a log level override path to give granular control over different logger's log levels
+#export PP_log_level_override_path=resources/log_level_override.json
+
+# An override for The log level for a JSON log
+#export PP_json_log_level=INFO
+
+# An override for The maximum size of a json log before it rotates, in bytes
+#export PP_json_log_maximum_bytes=
+
+# An override for Where static resources are
+#export PP_resource_path="resources/"
+
+# An override for The path to statistical thresholds
+#export PP_threshold_path="resources/thresholds/"
+
+# An override for The path to the application log configuration
+#export PP_log_config_path="resources/python_log_config.json
+
+# An override for Where to store intermediate files
+#export PP_intermediate_directory="intermediate"
+
+# An override for The default location for where to store results
+#export PP_output_directory=
+
+# An override for where to find profiles
+#export PP_profile_path="resources/profiles/"
+
+# An override for the maximum number of threads that may be used
+#export PP_MAXIMUM_ADDITIONAL_THREADS=3
+
+# An override for how verbose messages may be on a very granular level
+#export PP_VERBOSITY=0
