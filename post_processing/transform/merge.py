2a50d6abc28aff9dc27b01b3d192694fd4c0d2a0
Chris Tubbs <christopherotubbs@gmail.com>
2025-12-22 10:10:04 -0600

    Removed integer downcasting in file merges and added an additional encoding copy.

--- post_processing/transform/merge.py
+++ post_processing/transform/merge.py
@@ -35,70 +35,11 @@ def merge_files(files: generic.Sequence[str | pathlib.Path]) -> xarray.Dataset:
     LOGGER.debug(f"Merging {len(files)} files")
     combined_files: xarray.Dataset = load(target=files)
     LOGGER.debug(f"Data from {len(files)} have been merged. Now they are being encoded.")
-    for coordinate_name, variable in combined_files.coords.items():
-        new_dtype = variable.dtype
-
-        if variable.dtype in (numpy.int8, numpy.int16, numpy.int32, numpy.int64, numpy.uint16, numpy.uint32, numpy.uint64):
-            minimum = variable.min(skipna=True).item()
-            maximum = variable.max(skipna=True).item()
-
-            if isinstance(minimum, numpy.integer) and isinstance(maximum, numpy.integer):
-                unsigned: bool = minimum >= 0
-
-                if unsigned:
-                    if maximum < (2 ** 8):
-                        new_dtype = numpy.uint8
-                    elif maximum < (2 ** 16):
-                        new_dtype = numpy.uint16
-                    elif maximum <= (2 ** 32):
-                        new_dtype = numpy.uint32
-                    else:
-                        new_dtype = numpy.uint64
-                else:
-                    if -1 * ((2 ** 8) / 2) <= minimum and maximum < (2 ** 8) / 2:
-                        new_dtype = numpy.int8
-                    elif -1 * ((2 ** 16) / 2) <= minimum and maximum < (2 ** 16) / 2:
-                        new_dtype = numpy.int16
-                    elif -1 * ((2 ** 32) / 2) <= minimum and maximum < (2 ** 32) / 2:
-                        new_dtype = numpy.int32
-                    else:
-                        new_dtype = numpy.int64
-        else:
-            continue
-
-        if variable.dtype != new_dtype:
-            variable = variable.astype(new_dtype)
-            combined_files = combined_files.assign_coords({coordinate_name: variable})
 
     for variable_name, variable in combined_files.data_vars.items():
         new_dtype = variable.dtype
         if variable.dtype == numpy.float64:
             new_dtype = numpy.float32
-        elif variable.dtype in (numpy.int8, numpy.int16, numpy.int32, numpy.int64, numpy.uint16, numpy.uint32, numpy.uint64):
-            minimum = variable.min(skipna=True).item()
-            maximum = variable.max(skipna=True).item()
-
-            if isinstance(minimum, numpy.integer) and isinstance(maximum, numpy.integer):
-                unsigned: bool = minimum >= 0
-
-                if unsigned:
-                    if maximum < (2 ** 8):
-                        new_dtype = numpy.uint8
-                    elif maximum < (2 ** 16):
-                        new_dtype = numpy.uint16
-                    elif maximum <= (2 ** 32):
-                        new_dtype = numpy.uint32
-                    else:
-                        new_dtype = numpy.uint64
-                else:
-                    if -1 * ((2 ** 8) / 2) <= minimum and maximum < (2 ** 8) / 2:
-                        new_dtype = numpy.int8
-                    elif -1 * ((2 ** 16) / 2) <= minimum and maximum < (2 ** 16) / 2:
-                        new_dtype = numpy.int16
-                    elif -1 * ((2 ** 32) / 2) <= minimum and maximum < (2 ** 32) / 2:
-                        new_dtype = numpy.int32
-                    else:
-                        new_dtype = numpy.int64
 
         if variable.dtype != new_dtype:
             previous_encoding = variable.encoding.copy()
@@ -133,6 +74,7 @@ def merge_files(files: generic.Sequence[str | pathlib.Path]) -> xarray.Dataset:
                 name=variable.name
             )
             combined_files[variable_name] = simplified_variable
+            combined_files[variable_name].encoding.update(variable.encoding.copy())
 
         if variable.dtype == numpy.float64:
             previous_encoding: dict[str, typing.Any] = variable.encoding.copy()
